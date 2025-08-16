6e23e08b951d6f10c8a71dd92f84be9c516030ea
wayixia <wayixia@gmail.com>
2025-08-17 00:20:50 +0800

    add init script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # docker-web
-docker web server container
+使用docker解决nginx+php环境安装问题, 轻松部署网站服务器环境
+
+
+# 如何使用
+使用git拉取
+
+
---
+++ run_container_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+local_path=$(cd "$(dirname "$0")"; pwd)
+
+docker run -d --name lonydadev \
+	-v $local_path/app:/app \
+	-v $local_path/app/nginx-sites-enabled:/etc/nginx/sites-enabled \
+	-p 80:80 \
+	-p 9000:9000 \
+	php-nginx-dev:1.0
+
+
+#wayixia/php-nginx-dev:1.0
