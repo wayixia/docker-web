17663ad7901b78b7ee05c8e638d62ea5ede94443
wayixia <wayixia@gmail.com>
2025-08-18 00:00:02 +0800

    move nginx-sites-enabled to root directory

--- run_container_dev.sh
+++ run_container_dev.sh
@@ -3,9 +3,9 @@
 
 local_path=$(cd "$(dirname "$0")"; pwd)
 
-docker run -d --name lonydadev \
+docker run -d --name dockerwebdev \
 	-v $local_path/app:/app \
-	-v $local_path/app/nginx-sites-enabled:/etc/nginx/sites-enabled \
+	-v $local_path/nginx-sites-enabled:/etc/nginx/sites-enabled \
 	-p 80:80 \
 	-p 9000:9000 \
 	php-nginx-dev:1.0
