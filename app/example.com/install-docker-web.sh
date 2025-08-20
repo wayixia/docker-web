5e7dedd66f7b24de2336bc632be7d0f4f15ddbb9
lifeq <wayixia@gmail.com>
2025-08-20 23:20:05 +0800

    fix install-docker-web.sh bug

--- app/example.com/install-docker-web.sh
+++ app/example.com/install-docker-web.sh
@@ -8,11 +8,18 @@ filepath=$(cd "$(dirname "$0")"; pwd)
 appname=$(basename "$filepath")
 conf=$appname.conf
 
-echo "Install $current_dir to nginx server"
+
+
+echo "Restart docker-web server after installation completed"
+echo "======================================================"
+echo "Installing app $appname to nginx server"
 
 # 使用相对目录的创建软连接
 ln -s /app/$appname/nginx/$conf ../../nginx-sites-enabled/$conf
 
-
-echo "Restart docker-web server after installation completed"
+if [ $? -ne 0 ]; then
+  echo "发生错误。"
+else
+  echo "Installation is completed"
+fi
 
